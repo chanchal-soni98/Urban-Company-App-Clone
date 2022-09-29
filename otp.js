d91fbb7faa23020dbb0d1b21221b07bdeec0e146
rupesh gondrawar <101592209+rupeshgondrawar@users.noreply.github.com>
2022-09-30 03:43:16 +0530

    new

---
+++ otp.js
@@ -0,0 +1,14 @@
+let login=()=>{
+let ip1=document.getElementById("ip1").value;
+let ip2=document.getElementById("ip2").value;
+let ip3=document.getElementById("ip3").value;
+let ip4=document.getElementById("ip4").value;
+ 
+let obj={ip1,ip2,ip3,ip4};
+console.log(obj);
+localStorage.setItem("otp_data",JSON.stringify(obj));
+};
+
+let back=()=>{
+    window.location.href="login.html"
+};
