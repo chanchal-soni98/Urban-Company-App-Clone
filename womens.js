b7cc98ccea9a681bacf0a5091d2de7990bf35346
srinivas1152 <104242487+srinivas1152@users.noreply.github.com>
2022-10-01 20:43:31 +0530

    day-5

--- womens.js
+++ womens.js
@@ -3,7 +3,7 @@
 // let arr = JSON.parse(localStorage.getItem("user")) || []
 let url = "https://lit-wildwood-25489.herokuapp.com/api/vas"
 
- let cart_arr = JSON.parse(localStorage.getItem("cart"))||[]
+//  let cart_arr = JSON.parse(localStorage.getItem("cart"))||[]
  //console.log(cart_arr);
 
 let getData = async () => {
@@ -20,12 +20,16 @@ let render_dom = (data) => {
     let cont = document.getElementById("container1")
     cont.innerHTML = null;
 
-    data.forEach((el) => {
+    data.forEach((el , id) => {
    // console.log(el.head)
         let div = document.createElement("div")
         div.setAttribute("class", "womadiv")
+        let A9 = document.createElement("h4")
+        A9.innerText = el.package;
+        A9.setAttribute("class","wicket")
+        
         let h2 = document.createElement("h2")
-        h2.innerText = el.head;
+        h2.innerText = el.title;
         h2.setAttribute("class","he")
         let h5 = document.createElement("h5")
         h5.innerText = el.price;
@@ -39,70 +43,78 @@ let render_dom = (data) => {
 
 
         let p1 = document.createElement("p")
-        p1.innerText = el.com1;
+        p1.innerText = el.Last;
         p1.setAttribute("class","his")
         let p2 = document.createElement("p")
-        p2.innerText = el.com2;
+        p2.innerText = el.Last_1;
         p2.setAttribute("class","his")
         let p3 = document.createElement("p")
         p3.innerText = el.com3;
         p3.setAttribute("class","his")
         let btn = document.createElement("button")
-        btn.innerText = "add"
-        btn.setAttribute("id", "btn1")
+        btn.innerText = "Add"
+        btn.setAttribute("class", "btn1")
         btn.addEventListener("click", function () {
-            cart_fun(el)
+            cart_fun(el, id)
         })
         let h8 = document.createElement("hr")
-        h.setAttribute("id", "womah2")
+        h.setAttribute("class", "womah2")
 
-        div.append(h2, h5, btn, h6, h, p1, p2, p3, h8)
+        div.append( A9 ,h2, h5, btn, h6, h, p1, p2, p3, h8)
 
         cont.append(div)
 
     })
-    let cart_fun = (el) => {
-        
-        //")
-        //console.log(data)
-        //console.log(data.id)
-        //let cart = JSON.parse(localStorage.getItem("cart"))
-        //console.log(cart)
-        // let user=cart_arr
-        // if (user.includes(el)) {
-        //     alert("Item already in the cart")
-        // }
-        // else {
 
-        //     alert("successfully added to cart")
-        //     // console.log(cart_arr)
-        // }
-        // let cart_arr = JSON.parse(localStorage.getItem("cart"));
-        // let flag = false;
-        // cart_arr.map((el)=> {
-        //     if(el.head === data.head){
-        //         flag=true;
-                
-        //     }
-        // })
-        // if(flag){
-        //     alert("allready  in the cart")
-        //     return
-        // }
-        // cart_arr.push(el)
+    let cartLs = JSON.parse(localStorage.getItem("cart")) || []
+   
+    let cart_fun = (el , id) => {
         
-        // console.log(cart_arr)
+       
         
         localStorage.setItem("cart", JSON.stringify(el))
         alert("item added successfully")
-
+         
+    
 
         let fin = document.getElementById("woma_final")
         fin.setAttribute("class","last_b")
-        fin.innerHTML = cart_arr.price;
+        fin.innerHTML = cartLs.price;
+        let msg = document.getElementById("womamsg")
+        msg.setAttribute("class","womasree")
+        msg.innerText ="Congratulations! Rs450 saved so far!"
+
+
+        // let obj = {
+        //     package: el.package,
+        //     title: el.title,
+        //     price: el.price,
+        //     id: el.id,
+        //   };
+        //   let iscartAdded = false;
+        //   for (let i = 0; i < cartLs.length; i++) {
+        //     if (cartLs[i].id === obj.id) {
+        //       alert("product already in the Cart");
+        //       iscartAdded = true;
+        //       break;
+        //     }
+        //   }
+        //   if (iscartAdded === false) {
+        
+        //     cartLs.push(el);
+        //     alert("product Added Successfully");
+        //     localStorage.setItem("cart", JSON.stringify(cartLs));
+          }
+    
+
     }
 
-}
+    // let gocart = () => {
+    //     window.location.href="";
+    // }
+
+
+
 
 
 
